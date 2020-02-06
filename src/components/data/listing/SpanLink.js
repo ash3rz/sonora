dd6e17650f2869ef22554b1fc6e1fcd6e0df40c9
Ashley Ramsey <aramsey@cyverse.org>
2020-02-06 11:26:02 -0700

    Add ids and attributes for accessibility

--- src/components/data/listing/SpanLink.js
+++ src/components/data/listing/SpanLink.js
@@ -16,7 +16,7 @@ const useStyles = makeStyles(styles);
 
 function SpanLink(props) {
     const classes = useStyles();
-    const { onClick, children } = props;
+    const { onClick, id, children } = props;
 
     const keyboardHandler = (event) => {
         // if space key or enter key
@@ -27,6 +27,7 @@ function SpanLink(props) {
 
     return (
         <span
+            id={id}
             tabIndex={0}
             onClick={onClick}
             onKeyDown={keyboardHandler}
