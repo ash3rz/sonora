f30a2f4df77950a93fd1dee7979e6ebfff9f9512
Ash <aramsey@cyverse.org>
2021-06-24 11:38:18 -0700

    CORE-1400 WIP Maintenance page

---
+++ src/pages/maintenance.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+function MaintenancePage() {
+    return <Typography>We're in maintenance. Go home!</Typography>;
+}
+
+export default MaintenancePage;
